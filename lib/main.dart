e0523d523b412b07f2007edd24a064ddfdddf444
Revathy <revathymr916@gmail.com>
2024-04-08 11:06:37 +0530

    Map added

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+// import 'package:latlong2/latlong.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MaterialApp( home: MyApp()));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+   Widget build(BuildContext context) {
+    return FlutterMap(
+    mapController: MapController(),
+    options: const MapOptions(),
+    children: [
+      TileLayer(
+        urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+        userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+        // Plenty of other options available!
+),],
     );
-  }
+}
 }
